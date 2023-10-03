9591a87935fd370246713895ddf0b9f39150d111
dusagong <yoonseungjae123@gmail.com>
2023-10-03 14:22:24 +0900

    animatedSwitcher 코드는 짜놨는데 안돌아감

--- lib/screen/call.dart
+++ lib/screen/call.dart
@@ -13,34 +13,43 @@ class Call extends StatefulWidget {
 class _CallState extends State<Call> {
   bool isOverlayVisible = false;
   Timer? _overlayTimer; // Declare a Timer variable
+  late Widget mychoice;
+  double width_ = 1000.0; // Initialize with default values
+  double height_ = 2000.0;
+  @override
+  void initState() {
+    super.initState();
+    mychoice = b();
+  }
 
   void _toggleOverlay() {
     setState(() {
       isOverlayVisible = !isOverlayVisible;
+      isOverlayVisible ? mychoice = a() : mychoice = b();
     });
 
     if (isOverlayVisible) {
-      // Start a timer to hide the overlay after 3 seconds
-      _overlayTimer = Timer(Duration(seconds: 3), () {
+      _overlayTimer = Timer(Duration(seconds: 2), () {
         setState(() {
           isOverlayVisible = false;
+          mychoice = b();
         });
       });
     } else {
-      // If overlay is not visible, cancel the timer
       _overlayTimer?.cancel();
     }
   }
 
   @override
   void dispose() {
-    // Make sure to cancel the timer when the widget is disposed
     _overlayTimer?.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    width_ = MediaQuery.of(context).size.width;
+    height_ = MediaQuery.of(context).size.height;
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
@@ -54,92 +63,94 @@ class _CallState extends State<Call> {
         },
         child: Scaffold(
             backgroundColor: Colors.transparent,
-            body: isOverlayVisible
-                ? Stack(
-                    children: [
-                      Center(
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 500,
-                            ),
-                            Text(
-                                "              전화를 종료하고 싶으시면\n          잠금 또는 음량 버튼을 누르거나,\n   화면을 터치하여 종료하기 버튼을 누르세요",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xffE5DDEA),
-                                )),
-                            if (isOverlayVisible)
-                              GestureDetector(
-                                  onTap: () {
-                                    Get.offAll(Home());
-                                  },
-                                  child: Column(
-                                    children: [
-                                      SizedBox(
-                                        height: 55,
-                                      ),
-                                      Image.asset("assets/Call/closeMoon.png"),
-                                      Text(
-                                        "전화 종료하기",
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w400,
-                                          color:
-                                              Color.fromRGBO(229, 221, 234, 1),
-                                        ),
-                                      ),
-                                    ],
-                                  )),
-                          ],
-                        ),
-                      ),
-                    ],
-                  )
-                : Stack(
-                    children: [
-                      Center(
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 500,
-                            ),
-                            Text(
-                                "              전화를 종료하고 싶으시면\n          잠금 또는 음량 버튼을 누르거나,\n   화면을 터치하여 종료하기 버튼을 누르세요",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xffE5DDEA),
-                                )),
-                          ],
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _toggleOverlay();
-                          });
-                        },
-                        child: Container(
-                          color: Colors.black.withOpacity(
-                              0.5), // Black overlay with 50% opacity
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                        ),
-                      ),
-
-                      // Visibility(
-                      //   visible: isOverlayVisible,
-                      //   child: Container(
-                      //     color: Colors.black.withOpacity(1), // Black overlay with 50% opacity
-                      //     width: MediaQuery.of(context).size.width,
-                      //     height: MediaQuery.of(context).size.height,
-                      //   ),
-                      // ),
-                    ],
-                  )),
+            body: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 2500),
+              key: ValueKey<Widget>(mychoice),
+              child: mychoice,
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(scale: animation, child: child);
+              },
+            )),
       ),
+    );
+  }
+
+  Widget a() {
+    return Stack(
+      children: [
+        Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 520,
+              ),
+              Text(
+                  "              전화를 종료하고 싶으시면\n          잠금 또는 음량 버튼을 누르거나,\n   화면을 터치하여 종료하기 버튼을 누르세요",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xffE5DDEA),
+                  )),
+              if (isOverlayVisible)
+                GestureDetector(
+                    onTap: () {
+                      Get.offAll(Home());
+                    },
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 55,
+                        ),
+                        Image.asset("assets/Call/closeMoon.png"),
+                        Text(
+                          "전화 종료하기",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromRGBO(229, 221, 234, 1),
+                          ),
+                        ),
+                      ],
+                    )),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget b() {
+    return Stack(
+      children: [
+        Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 520,
+              ),
+              Text(
+                  "              전화를 종료하고 싶으시면\n          잠금 또는 음량 버튼을 누르거나,\n   화면을 터치하여 종료하기 버튼을 누르세요",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xffE5DDEA),
+                  )),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _toggleOverlay();
+            });
+          },
+          child: Container(
+              color: Colors.black
+                  .withOpacity(0.5), // Black overlay with 50% opacity
+              width: width_,
+              height: height_),
+        ),
+      ],
     );
   }
 }
